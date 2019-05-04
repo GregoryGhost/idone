1b2f8b719eca0dd417d1fb304b9726e4a0d12dfe
Gregory <KulakovGV@tomskasu.ru>
2019-05-05 01:28:50 +0700

    Добавил пример теста по взаимодействию language-ext Option'а с FSharp'овым.

---
+++ Idone/Idone.Security/EnterPoint.cs
@@ -0,0 +1,26 @@
+﻿namespace Idone.Security
+{
+    using LanguageExt;
+
+    using static LanguageExt.Prelude;
+
+    public class EnterPoint : ISecurityModule
+    {
+        public Option<Test> RegistrateUser(Test test)
+        {
+            return test != null
+                ? Some(test)
+                : None;
+        }
+    }
+
+    public interface ISecurityModule
+    {
+        Option<Test> RegistrateUser(Test test);
+    }
+
+    public class Test
+    {
+        public int Data { get; set; }
+    }
+}
--- Idone/Idone.Tests/Sample.fs
+++ Idone/Idone.Tests/Sample.fs
@@ -1,36 +1,23 @@
 module Tests
 
 open Expecto
+open Idone.Security
+open LanguageExt
 
 [<Tests>]
 let tests =
-  testList "samples" [
-    testCase "universe exists (╭ರᴥ•́)" <| fun _ ->
-      let subject = true
-      Expect.isTrue subject "I compute, therefore I am."
+  testList "Модуль админки" [
+    test "Option Some language-ext'а" {
+      let security = new EnterPoint()
+      let actual = security.RegistrateUser(new Test()) |> FSharp.fs
 
-    testCase "when true is not (should fail)" <| fun _ ->
-      let subject = false
-      Expect.isTrue subject "I should fail because the subject is false"
+      Expect.isSome actual "Не совпали"
+    }    
+    
+    test "Option None language-ext'а" {
+      let security = new EnterPoint()
+      let actual = security.RegistrateUser(null) |> FSharp.fs
 
-    testCase "I'm skipped (should skip)" <| fun _ ->
-      Tests.skiptest "Yup, waiting for a sunny day..."
-
-    testCase "I'm always fail (should fail)" <| fun _ ->
-      Tests.failtest "This was expected..."
-
-    testCase "contains things" <| fun _ ->
-      Expect.containsAll [| 2; 3; 4 |] [| 2; 4 |]
-                         "This is the case; {2,3,4} contains {2,4}"
-
-    testCase "contains things (should fail)" <| fun _ ->
-      Expect.containsAll [| 2; 3; 4 |] [| 2; 4; 1 |]
-                         "Expecting we have one (1) in there"
-
-    testCase "Sometimes I want to ༼ノಠل͟ಠ༽ノ ︵ ┻━┻" <| fun _ ->
-      Expect.equal "abcdëf" "abcdef" "These should equal"
-
-    test "I am (should fail)" {
-      "╰〳 ಠ 益 ಠೃ 〵╯" |> Expect.equal true false
+      Expect.isNone actual "Не совпали"
     }
   ]
