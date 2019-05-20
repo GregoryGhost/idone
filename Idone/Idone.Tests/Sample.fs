61dee5b49e02bab4b42d4cac6f2aa3e2ded421d6
Gregory <KulakovGV@tomskasu.ru>
2019-05-20 15:27:51 +0700

    Добавил тест по регистрации пользователя в системе.

--- Idone/Idone.Tests/Sample.fs
+++ Idone/Idone.Tests/Sample.fs
@@ -7,9 +7,11 @@ module Tests =
     open Idone.Tests.Extensions
     open LanguageExt
 
+    let private _security = new EnterPoint() :> ISecurityModule
+
     [<Tests>]
     let tests =
-      testList "Модуль админки" [
+      testList "Модуль админки" [       
         test "Option Some language-ext'а" {
           let security = new EnterPoint()
           let actual = security.RegistrateUser(new Test()) |> FSharp.fs
@@ -45,6 +47,41 @@ module Tests =
           let expected = new Test(666)
 
           Expect.areEqual actual expected "Не совпали"
+        }
+
+        test "Регистрация нового пользователя" {
+            //1.Найти пользователя в домене (берем хардкод данные)
+            //2.Получить данные требуемого пользователя у AD сервера
+            //3.Передать данные пользователя на регистрацию в системе
+            //4.Найти зареганного пользователя в системе (в гриде всех пользователей)
+          
+          let findDomainUser =
+            _security.FindUserByDisplayName
+
+          let fillUserCredentials userData =
+            new DtoRegistrateUser(userData.Sid,
+                userData.Surname,
+                userData.Name,
+                userData.Patronomyc,
+                userData.Email)
+          
+          let registrateUser =
+            _security.RegistrateUser
+
+          let findRegistratedUser user =
+            user.Email |> _security.GetUserGrid
+            >> getUsers //DtoUser list
+            >> List.filter (fun u -> u.Email = user.Email) 
+
+          //user cases
+          let searchExpression = "Кулаков*"
+          let registratedUser = 
+              searchExpression |> findDomainUser
+              >> fillUserCredentials
+              >> registrateUser
+              >> findRegistratedUser
+
+          Expect.isRight registratedUser "Пользователь не зарегистрирован"
         }
         //TODO: добавить тесты для проверки работы с ролями, правами, пользователями
       ]
