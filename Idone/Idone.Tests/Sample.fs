193a3cbc471e9839a11c81cf6d3842ea101a51e0
Gregory <KulakovGV@tomskasu.ru>
2019-05-21 20:50:47 +0700

    Обновил заготовку тестов по регистрации пользователя.

--- Idone/Idone.Tests/Sample.fs
+++ Idone/Idone.Tests/Sample.fs
@@ -9,46 +9,13 @@ module Tests =
 
     let private _security = new EnterPoint() :> ISecurityModule
 
+    let (>>=) (x : Either<'L,'R>) (f: ('R -> Either<'L, 'B>)) =
+        x.Bind(f)
+
+
     [<Tests>]
     let tests =
       testList "Модуль админки" [       
-        test "Option Some language-ext'а" {
-          let security = new EnterPoint()
-          let actual = security.RegistrateUser(new Test()) |> FSharp.fs
-
-          Expect.isSome actual "Не совпали"
-        }    
-    
-        test "Option None language-ext'а" {
-          let security = new EnterPoint()
-          let actual = security.RegistrateUser(null) |> FSharp.fs
-
-          Expect.isNone actual "Не совпали"
-        }
-
-        test "Either Right language-ext'а" {
-          let security = new EnterPoint()
-          let actual = security.RegistrateUserEither(new Test())
-
-          Expect.isRight actual "Не совпали"
-        }    
-    
-        test "Either Left language-ext'а" {
-          let security = new EnterPoint()
-          let actual = security.RegistrateUserEither(null)
-
-          Expect.isLeft actual "Не совпали"
-        }    
-
-        test "Either areEqual Records language-ext'а" {
-          let security = new EnterPoint()
-          let test = new Test(666)
-          let actual = security.RegistrateUser2(test)
-          let expected = new Test(666)
-
-          Expect.areEqual actual expected "Не совпали"
-        }
-
         test "Регистрация нового пользователя" {
             //1.Найти пользователя в домене (берем хардкод данные)
             //2.Получить данные требуемого пользователя у AD сервера
@@ -58,28 +25,44 @@ module Tests =
           let findDomainUser =
             _security.FindUserByDisplayName
 
-          let fillUserCredentials userData =
-            new DtoRegistrateUser(userData.Sid,
+          let fillUserCredentials (userData : DtoAdUser) : DtoRegistrateUser =
+            new DtoRegistrateUser(
+                userData.Sid,
                 userData.Surname,
                 userData.Name,
-                userData.Patronomyc,
+                userData.Patronomic,
                 userData.Email)
           
           let registrateUser =
             _security.RegistrateUser
 
-          let findRegistratedUser user =
-            user.Email |> _security.GetUserGrid
-            >> getUsers //DtoUser list
-            >> List.filter (fun u -> u.Email = user.Email) 
+          let getUsers (gridUser : DtoGridUser) : DtoRowUser seq = 
+            gridUser.Rows
+
+          let FIRST_PAGE = new Pagination(10, 1)
+
+          let fillGridQueryUser (user : DtoUserFilter) : DtoGridQueryUser =
+            let filter = new DtoUserFilter(user.Email)
+            let query = new DtoGridQueryUser(filter, FIRST_PAGE)
+            query
+
+          let toEither (x : 'R option) (error : 'L) : Either<'L, 'R> =
+            match x with
+            | Some y -> LanguageExt.Prelude.Right<'L, 'R>(y)
+            | None -> LanguageExt.Prelude.Left<'L, 'R>(error)
+          let findRegistratedUser (user : DtoRegistratedUser) : Either<Error, DtoRowUser> =
+            let convert x = toEither x Error.Exception
+            let findUser = getUsers >> Seq.tryFind (fun u -> u.Email = user.Email) >> convert
+            let gridQueryUser = new DtoUserFilter(user.Email) |> fillGridQueryUser
+            gridQueryUser |> _security.GetGridUser >>= findUser
 
           //user cases
           let searchExpression = "Кулаков*"
-          let registratedUser = 
-              searchExpression |> findDomainUser
-              >> fillUserCredentials
-              >> registrateUser
-              >> findRegistratedUser
+          let registratedUser : Either<Error, DtoRowUser> = 
+            searchExpression 
+            |> findDomainUser 
+            >>= (fillUserCredentials >> registrateUser) 
+            >>= findRegistratedUser
 
           Expect.isRight registratedUser "Пользователь не зарегистрирован"
         }
